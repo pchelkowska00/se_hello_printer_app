0c31177437d5cc5d5c0fdfe5cf423fc9c11dd948
pchelkowska00 <pchelkowska00@users.noreply.github.com>
2018-05-05 14:54:08 +0200

    ostateczny

--- test_ui/test_ui2.py
+++ test_ui/test_ui2.py
@@ -11,5 +11,7 @@ class TestFormater(unittest.TestCase):
     def test_plain_lowercase(self):
         driver = webdriver.Chrome()
         driver.get("http://127.0.0.1:5000/ui")
+        link=driver.find_element_by_xpath("/html/body/a")
+        link.click()
         time.sleep (100)
         driver.quit()
